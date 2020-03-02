272fc78260adb5250e3feec3bd138ac2b04bb143
Alexandre Pedrecal <pedrecalc@gmail.com>
2020-03-02 20:41:18 -0300

    :heavy_plus_sign: add dotenv

--- src/start.js
+++ src/start.js
@@ -1,6 +1,8 @@
+require('dotenv').config({ path: 'variables.env' });
+
 const app = require('./app');
 
-app.set('port', process.env.PORT || 1138);
+app.set('port', process.env.PORT || 7023);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
